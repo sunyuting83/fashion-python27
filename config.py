a9ff5d5d3cffb0a42c6dd3e4eceb3be01d4aea51
sunyuting83 <sleepsun123@163.com>
2018-11-22 22:31:41 +0800

    modify config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ import json
 # 	return setting
 
 class Config(object):
-	SECRET_KEY = 's9oV39FEtQ4A2cHN3AU2dRFDpegaEVgLzZOYKbdATD0oeHcPOmRgKzphIoBRVQSI'
+	SECRET_KEY = '4Z7naifZ8uPJnHHXQaR4LoUIlrI8eUA6MocxtLV5WrL3KqAFk177QIXbryYa2eySlX7sUuqrSH'
 
 
 # json_config = loadConfig()
@@ -18,22 +18,22 @@ class LocalhostConfig(Config):
 	REDIS_HOST = '127.0.0.1'
 	REDIS_PORT = 6379
 	REDIS_DB = 4
-	REDIS_PASSWORD = '3SuLc0VvzLJm'
+	REDIS_PASSWORD = 'BCnb1jzIzHW9'
 
-	MYSQL_INFO = "mysql://root:penny5921929@127.0.0.1:3306/fashion?charset=utf8"
+	MYSQL_INFO = "mysql://root:root@127.0.0.1:3306/fashion?charset=utf8"
 	SERVER_URL = "http://localhost:5000/"
 
 	SQLALCHEMY_DATABASE_URI = MYSQL_INFO
 	SQLALCHEMY_TRACK_MODIFICATIONS = True
 	SQLALCHEMY_COMMIT_ON_TEARDOWN =True
 
-	YunPian_APIKEY = "akfkxr6e9f5r4wqle4borkwwpoejmdyu" # 云片网短信接口API Key
+	YunPian_APIKEY = "sDysNShY76MiSZhJmp3rtsFfOZabNrPo" # 云片网短信接口API Key
 
 	MAIL_SERVER = "smtp.mxhichina.com"  # 邮件服务器地址
 	MAIL_PORT = 465			   # 邮件服务器端口
 	MAIL_USE_SSL = True
 	MAIL_USE_TLS = False		  # 启用 TLS
 	MAIL_USERNAME = "ceshi@163.com"
-	MAIL_PASSWORD = "d39LE5arbMFn"
+	MAIL_PASSWORD = "maKjRcJwz7Z5"
 
 Conf = LocalhostConfig
