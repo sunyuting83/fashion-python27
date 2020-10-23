2780653b254c7cca19b838aff5053a56ec2f9180
sunyuting83 <sleepsun123@163.com>
2020-10-23 23:29:45 +0800

    use CDN for static file

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
 # coding:utf-8
-
+from meinheld import patch
+patch.patch_all()
+from meinheld import server
+from meinheld import middleware
 from flask import Flask
 
 from uploader import Uploader
@@ -33,4 +36,6 @@ if __name__ == '__main__':
 	def hello_world():
 		return 'Hello World!'
 
-	app.run(debug=True, host='0.0.0.0', port=5000, threaded=True)
+	# app.run(debug=True, host='0.0.0.0', port=5000, threaded=True)
+	server.listen(("0.0.0.0", 5000))
+	server.run(middleware.ContinuationMiddleware(app))
